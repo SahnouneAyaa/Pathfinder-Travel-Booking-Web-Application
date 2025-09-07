705bfc4a5aa62e70b7a2d4b1907a99a99f754443
SahnouneAyaa <sahnouneaya15@gmail.com>
2025-09-07 16:22:26 +0100

    first commit

---
+++ src/app/blog/[...slug]/page.tsx
@@ -0,0 +1,228 @@
+import React from "react";
+import { DEMO_POSTS } from "@/data/posts";
+import { PostDataType } from "@/data/types";
+import Avatar from "@/shared/Avatar";
+import Badge from "@/shared/Badge";
+import ButtonPrimary from "@/shared/ButtonPrimary";
+import ButtonSecondary from "@/shared/ButtonSecondary";
+import Comment from "@/shared/Comment";
+import SocialsList from "@/shared/SocialsList";
+import Textarea from "@/shared/Textarea";
+import Image from "next/image";
+import travelhero2Image from "@/images/travelhero2.png";
+import Link from "next/link";
+import { Route } from "@/routers/types";
+
+const Page = ({
+  params,
+  searchParams,
+}: {
+  params: { stepIndex: string };
+  searchParams?: { [key: string]: string | string[] | undefined };
+}) => {
+  const renderHeader = () => {
+    return (
+      <header className="container rounded-xl">
+        <div className="max-w-screen-md mx-auto space-y-5">
+          <Badge href="/blog" color="purple" name="Traveler" />
+          <h1
+            className=" text-neutral-900 font-semibold text-3xl md:text-4xl md:!leading-[120%] lg:text-4xl dark:text-neutral-100 max-w-4xl "
+            title="Quiet ingenuity: 120,000 lunches and counting"
+          >
+            Keep up the spirit of the desire to travel around the world
+          </h1>
+          <span className="block text-base text-neutral-500 md:text-lg dark:text-neutral-400 pb-1">
+            We’re an online magazine dedicated to covering the best in
+            international product design. We started as a little blog back in
+            2002 covering student work and over time
+          </span>
+
+          <div className="w-full border-b border-neutral-100 dark:border-neutral-800"></div>
+          <div className="flex flex-col items-baseline sm:flex-row sm:justify-between">
+            <div className="nc-PostMeta2 flex items-center flex-wrap text-neutral-700 text-left dark:text-neutral-200 text-sm leading-none flex-shrink-0">
+              <Avatar
+                containerClassName="flex-shrink-0"
+                sizeClass="w-8 h-8 sm:h-11 sm:w-11 "
+              />
+              <div className="ml-3">
+                <div className="flex items-center">
+                  <a className="block font-semibold" href="/">
+                    Fones Mimi
+                  </a>
+                </div>
+                <div className="text-xs mt-[6px]">
+                  <span className="text-neutral-700 dark:text-neutral-300">
+                    May 20, 2021
+                  </span>
+                  <span className="mx-2 font-semibold">·</span>
+                  <span className="text-neutral-700 dark:text-neutral-300">
+                    6 min read
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="mt-3 sm:mt-0 sm:ml-3">
+              <SocialsList />
+            </div>
+          </div>
+        </div>
+      </header>
+    );
+  };
+
+  const renderTags = () => {
+    return (
+      <div className="max-w-screen-md mx-auto flex flex-wrap">
+        <a
+          className="nc-Tag inline-block bg-white text-sm text-neutral-600 dark:text-neutral-300 py-2 rounded-lg border border-neutral-100  md:px-4 dark:bg-neutral-700 dark:border-neutral-700 hover:border-neutral-200 dark:hover:border-neutral-6000 mr-2 mb-2"
+          href="##"
+        >
+          Garden
+        </a>
+        <a
+          className="nc-Tag inline-block bg-white text-sm text-neutral-600 dark:text-neutral-300 py-2 rounded-lg border border-neutral-100  md:px-4 dark:bg-neutral-700 dark:border-neutral-700 hover:border-neutral-200 dark:hover:border-neutral-6000 mr-2 mb-2"
+          href="##"
+        >
+          Jewelry
+        </a>
+        <a
+          className="nc-Tag inline-block bg-white text-sm text-neutral-600 dark:text-neutral-300 py-2 rounded-lg border border-neutral-100  md:px-4 dark:bg-neutral-700 dark:border-neutral-700 hover:border-neutral-200 dark:hover:border-neutral-6000 mr-2 mb-2"
+          href="##"
+        >
+          Tools
+        </a>
+      </div>
+    );
+  };
+
+  const renderAuthor = () => {
+    return (
+      <div className="max-w-screen-md mx-auto ">
+        <div className="nc-SingleAuthor flex">
+          <Avatar sizeClass="w-11 h-11 md:w-24 md:h-24" />
+          <div className="flex flex-col ml-3 max-w-lg sm:ml-5 space-y-1">
+            <span className="text-xs text-neutral-400 uppercase tracking-wider">
+              WRITEN BY
+            </span>
+            <h2 className="text-lg font-semibold text-neutral-900 dark:text-neutral-200">
+              <a href="/">Fones Mimi</a>
+            </h2>
+            <span className="text-sm text-neutral-500 sm:text-base dark:text-neutral-300">
+                Found some amazing deals here. Highly recommend for anyone looking to book their next travel adventure.
+              <a className="text-primary-6000 font-medium ml-1" href="/">
+                Readmore
+              </a>
+            </span>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const renderCommentForm = () => {
+    return (
+      <div className="max-w-screen-md mx-auto pt-5">
+        <h3 className="text-xl font-semibold text-neutral-800 dark:text-neutral-200">
+          Responses (14)
+        </h3>
+        <form className="nc-SingleCommentForm mt-5">
+          <Textarea />
+          <div className="mt-2 space-x-3">
+            <ButtonPrimary>Submit</ButtonPrimary>
+            <ButtonSecondary>Cancel</ButtonSecondary>
+          </div>
+        </form>
+      </div>
+    );
+  };
+
+  const renderCommentLists = () => {
+    return (
+      <div className="max-w-screen-md mx-auto">
+        <ul className="nc-SingleCommentLists space-y-5">
+          <li>
+            <Comment />
+            <ul className="pl-4 mt-5 space-y-5 md:pl-11">
+              <li>
+                <Comment isSmall />
+              </li>
+            </ul>
+          </li>
+          <li>
+            <Comment />
+            <ul className="pl-4 mt-5 space-y-5 md:pl-11">
+              <li>
+                <Comment isSmall />
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    );
+  };
+
+  const renderPostRelated = (post: PostDataType) => {
+    return (
+      <div
+        key={post.id}
+        className="relative aspect-w-3 aspect-h-4 rounded-3xl overflow-hidden group"
+      >
+        <Link href={post.href as Route} />
+        <Image
+          className="object-cover transform group-hover:scale-105 transition-transform duration-300"
+          src={post.featuredImage}
+          fill
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
+          alt=""
+        />
+        <div>
+          <div className="absolute bottom-0 inset-x-0 h-1/2 bg-gradient-to-t from-black"></div>
+        </div>
+        <div className="flex flex-col justify-end items-start text-xs text-neutral-300 space-y-2.5 p-4">
+          <Badge name="Categories" />
+          <h2 className="block text-lg font-semibold text-white ">
+            <span className="line-clamp-2">{post.title}</span>
+          </h2>
+
+          <div className="flex">
+            <span className="block text-neutral-200 hover:text-white font-medium truncate">
+              {post.author.displayName}
+            </span>
+            <span className="mx-1.5 font-medium">·</span>
+            <span className="font-normal truncate">{post.date}</span>
+          </div>
+        </div>
+        <Link href={post.href as Route} />
+      </div>
+    );
+  };
+
+  return (
+    <div className="nc-PageSingle pt-8 lg:pt-16 ">
+      {renderHeader()}
+      <div className="container my-10 sm:my-12 ">
+        <Image className="w-full rounded-xl" src={travelhero2Image} alt="" />
+      </div>
+
+      <div className="nc-SingleContent container space-y-10">
+        {renderTags()}
+        <div className="max-w-screen-md mx-auto border-b border-t border-neutral-100 dark:border-neutral-700"></div>
+        {renderAuthor()}
+        {renderCommentForm()}
+        {renderCommentLists()}
+      </div>
+      <div className="relative bg-neutral-100 dark:bg-neutral-800 py-16 lg:py-28 mt-16 lg:mt-24">
+        <div className="container ">
+          <h2 className="text-3xl font-semibold">Related posts</h2>
+          <div className="mt-10 grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 md:gap-8">
+            {/*  */}
+            {DEMO_POSTS.filter((_, i) => i < 4).map(renderPostRelated)}
+            {/*  */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/shared/Logo.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import logoImg from "@/images/logo.png";
+import logoLightImg from "@/images/logo-light.png";
+import LogoSvgLight from "./LogoSvgLight";
+import LogoSvg from "./LogoSvg";
+import Link from "next/link";
+import { StaticImageData } from "next/image";
+
+export interface LogoProps {
+  img?: StaticImageData;
+  imgLight?: StaticImageData;
+  className?: string;
+}
+
+const Logo: React.FC<LogoProps> = ({
+  img = logoImg,
+  imgLight = logoLightImg,
+  className = "w-24",
+}) => {
+  return (
+    <Link
+      href="/"
+      className={`ttnc-logo inline-block text-primary-6000 focus:outline-none focus:ring-0 ${className}`}
+    >
+        <span style={{fontSize:"25px", fontFamily:"bold"}}>PathFinder</span>
+    </Link>
+  );
+};
+
+export default Logo;
